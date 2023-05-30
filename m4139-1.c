5ac5364c410a2f3fc54fc77cf554b3800b6b8b12
jasoliyajishan <132878572+jasoliyajishan@users.noreply.github.com>
2023-05-30 12:42:04 +0530

    m4139-1.c

--- m4139-1.c
+++ m4139-1.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 
-main()
+ main() 
+ {
+   int i, a[100], n;
 
-{
-	char ch='a';
-	while(ch<='z')
+   printf("\nEnter no of elements :");
+   scanf("%d", &n);
+
+   printf("\nEnter the values :");
+   for (i = 0; i < n; i++)
     {
-    printf("%c",ch)	;
-    ch++;
-	}	
+      scanf("%d", &a[i]);
+   }
+
+   for (i = 0; i < n; i++) 
+   {
+      printf("\na[%d] = %d", i, a[i]);
+   }
+ 
+
 }
